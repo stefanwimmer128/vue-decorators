539b2908da6fafe178b83948bdac641844190648
Stefan Wimmer <info@stefanwimmer128.eu>
2019-06-03 10:19:08 +0200

    fix graphql types

--- packages/@vue-decorators/apollo/src/SmartQuery.ts
+++ packages/@vue-decorators/apollo/src/SmartQuery.ts
@@ -3,25 +3,25 @@ import {
 } from "graphql";
 import {
     ApolloVueThisType,
-    ExtendableVueApolloQueryOptions,
+    VueApolloQueryOptions,
 } from "vue-apollo/types/options";
 import {
     createDecorator,
     VueDecorator,
 } from "vue-class-component";
 
-interface VueApolloQueryOptions<V, R, T> extends ExtendableVueApolloQueryOptions<V, R> {
+interface SmartQueryOptions<V, R, T> extends VueApolloQueryOptions<V, R> {
     query: ((this: ApolloVueThisType<V>) => DocumentNode) | DocumentNode;
     variables?: ((this: ApolloVueThisType<V>) => T | T);
     client?: string;
 }
 
-type VueApolloQueryOptionsType<V, R, T> = VueApolloQueryOptions<V, R, T> | ((this: ApolloVueThisType<V>) => VueApolloQueryOptions<V, R, T>);
+type CallableSmartQueryOptions<V, R, T> = SmartQueryOptions<V, R, T> | ((this: ApolloVueThisType<V>) => SmartQueryOptions<V, R, T>);
 
 export default function SmartQuery(query: DocumentNode): VueDecorator;
-export default function SmartQuery<V, R = any, T = any>(options: VueApolloQueryOptionsType<V, R, T>): VueDecorator;
+export default function SmartQuery<V, R = any, T = any>(options: CallableSmartQueryOptions<V, R, T>): VueDecorator;
 
-export default function SmartQuery<V, R = any, T = any>(options: DocumentNode | VueApolloQueryOptionsType<V, R, T>) {
+export default function SmartQuery<V, R = any, T = any>(options: DocumentNode | CallableSmartQueryOptions<V, R, T>) {
     return createDecorator((cOptions: any, key) => {
         (cOptions.apollo = cOptions.apollo || {})[key] = options;
     });
