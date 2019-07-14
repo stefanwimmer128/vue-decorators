0b015da777a8ef47d7a9c785fd18c2936321ef08
Stefan Wimmer <info@stefanwimmer128.eu>
2019-07-14 18:10:58 +0200

    Update deps

--- packages/@vue-decorators/all/src/index.ts
+++ packages/@vue-decorators/all/src/index.ts
@@ -1,8 +1,6 @@
 export * from "@vue-decorators/apollo";
 export * from "@vue-decorators/types";
 
-export {
-    createDecorator,
-} from "vue-class-component";
+export * from "vue-class-component";
 export * from "vue-property-decorator";
 export * from "vuex-class";
