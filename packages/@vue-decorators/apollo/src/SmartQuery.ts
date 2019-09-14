3ed285aae45c40a5e25fdf7810d0a4d5172e8ad4
Stefan Wimmer <info@stefanwimmer128.eu>
2019-09-14 17:16:35 +0200

    Update deps

--- packages/@vue-decorators/apollo/src/SmartQuery.ts
+++ packages/@vue-decorators/apollo/src/SmartQuery.ts
@@ -2,31 +2,31 @@ import {
     DocumentNode,
 } from "graphql";
 import {
-    ApolloVueThisType,
-    VueApolloQueryOptions,
+    VueApolloQueryDefinition,
 } from "vue-apollo/types/options";
 import {
     createDecorator,
     VueDecorator,
 } from "vue-class-component";
 
+import VueThisType from "@vue-decorators/types/lib/VueThisType";
 import VariableObjectType from "@vue-decorators/types/lib/VariableObjectType";
 import VueThisCallable from "@vue-decorators/types/lib/VueThisCallable";
 
-export interface SmartQueryOptions<V, T, R extends VariableObjectType, I extends VariableObjectType> extends VueApolloQueryOptions<V, R> {
-    query: ((this: ApolloVueThisType<V>) => DocumentNode) | DocumentNode;
-    variables?: I | ((this: ApolloVueThisType<V>) => I);
-    update?: (this: ApolloVueThisType<V>, data: R) => T;
+export interface SmartQueryOptions<V, T, R extends VariableObjectType, I extends VariableObjectType> extends VueApolloQueryDefinition<V, R> {
+    query: ((this: VueThisType<V>) => DocumentNode) | DocumentNode;
+    variables?: I | ((this: VueThisType<V>) => I);
+    update?: (this: VueThisType<V>, data: R) => T;
     client?: string;
 }
 
-export type CallableSmartQueryOptions<V, T, R extends VariableObjectType, I extends VariableObjectType> = SmartQueryOptions<V, T, R, I> | ((this: ApolloVueThisType<V>) => SmartQueryOptions<V, T, R, I>);
+export type CallableSmartQueryOptions<V, T, R extends VariableObjectType, I extends VariableObjectType> = SmartQueryOptions<V, T, R, I> | ((this: VueThisType<V>) => SmartQueryOptions<V, T, R, I>);
 
 export default function SmartQuery(query: DocumentNode): VueDecorator;
 export default function SmartQuery<V = any, T = any, R extends VariableObjectType = VariableObjectType, I extends VariableObjectType = VariableObjectType>(options: VueThisCallable<V, SmartQueryOptions<V, T, R, I>>): VueDecorator;
 
 export default function SmartQuery<V = any, T = any, R extends VariableObjectType = VariableObjectType, I extends VariableObjectType = VariableObjectType>(options: DocumentNode | VueThisCallable<V, SmartQueryOptions<V, T, R, I>>) {
     return createDecorator((cOptions: any, key) => {
-        (cOptions.apollo = cOptions.apollo || {})[key] = options;
+        (cOptions.apollo || (cOptions.apollo = {}))[key] = options;
     });
 }
--- packages/@vue-decorators/apollo/src/SubscribeToMore.ts
+++ packages/@vue-decorators/apollo/src/SubscribeToMore.ts
@@ -1,31 +1,21 @@
 import {
-    DocumentNode,
-    ExecutionResult,
-} from "graphql";
-import {
-    ApolloVueThisType,
-} from "vue-apollo/types/options";
+    SubscribeToMoreOptions as ApolloSubscribeToMoreOptions,
+} from "apollo-client/core/watchQueryOptions";
 import {
     createDecorator,
     VueDecorator,
 } from "vue-class-component";
 
+import VueThisType from "@vue-decorators/types/lib/VueThisType";
 import VariableObjectType from "@vue-decorators/types/lib/VariableObjectType";
 import VueThisCallable from "@vue-decorators/types/lib/VueThisCallable";
 
-export interface SubscribeToMoreOptions<V, R extends VariableObjectType, S extends VariableObjectType, I extends VariableObjectType> {
-    document: DocumentNode;
-    variables?: I | ((this: ApolloVueThisType<V>) => I);
-    updateQuery?: (this: ApolloVueThisType<V>, previousQueryResult: R, options: {
-        error: any,
-        subscriptionData:  ExecutionResult<S>;
-        variables?: I;
-    }) => R;
-    onError?: (error: Error) => void;
+export interface SubscribeToMoreOptions<V, R = any, S = R, I = VariableObjectType> extends Omit<ApolloSubscribeToMoreOptions<R, I, S>, "variables"> {
+    variables?: I | ((this: VueThisType<V>) => I);
 }
 
-export default function SubscribeToMore<V = any, R = any, S = any, I = VariableObjectType>(options: VueThisCallable<V, SubscribeToMoreOptions<V, R, S, I>>): VueDecorator {
-    return createDecorator((cOptions: any, key) => {
-        cOptions.apollo[key].subscribeToMore = options;
+export default function SubscribeToMore<V = any, R = any, S = any, I = VariableObjectType>(apolloOptions: VueThisCallable<V, SubscribeToMoreOptions<V, R, S, I>>): VueDecorator {
+    return createDecorator((options: any, key) => {
+        options.apollo[key].subscribeToMore = apolloOptions;
     });
 }
